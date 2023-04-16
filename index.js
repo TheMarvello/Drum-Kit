e5e6b74bc09adf8521edfbb1efbb19213ae8115e
TheMarvello <dipukumars7476@gmail.com>
2023-04-16 21:29:09 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,78 @@
+//detecting mouse button click
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+
+    function handleClick() {
+        var buttonInnerHtml = this.innerHTML;
+        makesound(buttonInnerHtml);
+        buttonAnimatio(buttonInnerHtml);
+    }
+}
+
+
+//detecting keyboard keys press
+document.addEventListener("keydown", function(event) {
+    var key = event.key;
+    // console.log(key);
+    makesound(key);
+    buttonAnimatio(key);
+});
+
+
+//makesound function
+function makesound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "a":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        case "s":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "d":
+            var tom_1 = new Audio('sounds/tom-1.mp3');
+            tom_1.play();
+            break;
+
+        case "j":
+            var tom_2 = new Audio('sounds/tom-2.mp3');
+            tom_2.play();
+            break;
+
+        case "k":
+            var tom_3 = new Audio('sounds/tom-2.mp3');
+            tom_3.play();
+            break;
+
+        case "l":
+            var tom_4 = new Audio('sounds/tom-3.mp3');
+            tom_4.play();
+            break;
+
+        default:
+            console.log(key);
+            break;
+    }
+}
+
+//amimation
+
+function buttonAnimatio(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100)
+
+
+}
